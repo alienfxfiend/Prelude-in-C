aa81296d0d5d4919c162bfbd1bf43e11f19d01a9
Älïëñ FX Fïëñd™ <32994445+alienfxfiend@users.noreply.github.com>
2025-11-29 07:08:11 +0530

    New Read-Only Mode (Persistent Toggle) + Dirty Bit Flag to Prevent Unnecessary Saves

---
+++ Doodle-D2D-HAcc/Dirty Bit Flag Added +  Read Only Mode (Serialized) V5/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Doodle-D2D-HAcc.rc
+//
+#define IDI_ICON1                       101
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "dwrite.lib")
+
+// Menu Command IDs
+#define IDM_FILE_READONLY 1000 // New ID (Placed before Save)
+#define IDM_FILE_SAVE     1001
+#define IDM_FILE_EXIT     1002
+#define IDM_MODE_BRUSH    2001
+#define IDM_MODE_ERASER   2002
+#define IDM_TOOL_COLOR    2003
+#define IDM_SIZE_INC      2004
+#define IDM_SIZE_DEC      2005
+#define IDM_TOOL_CLEAR    2006
+#define IDM_GRID_TOGGLE   3001
+#define IDM_GRID_ALPHA    3002
+#define IDM_GRID_ZIN      3003
+#define IDM_GRID_ZOUT     3004
+#define IDM_OP_INC        3005
+#define IDM_OP_DEC        3006
+#define IDM_HELP_KEYS     4001
+#define IDM_HELP_ABOUT    4002
+
+ //----------------------------------------------------------------
+ // Data Structures and Globals
+ //----------------------------------------------------------------
